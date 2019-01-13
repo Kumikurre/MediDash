5838b55dd07c6d9c8e71dce2d8b0aee102522397
Atte Jauhiainen <atte.jauhiainen@gmail.com>
2019-01-13 02:46:43 +0200

    change server to flask server

--- medidash.py
+++ medidash.py
@@ -1,3 +1,6 @@
+from datetime import datetime as dt
+
+import flask
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,16 +11,12 @@ import dash_html_components as html
 import plotly.plotly as py
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-from datetime import datetime as dt
 
-app = dash.Dash(__name__)
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
 app.css.config.serve_locally = True
 app.scripts.config.serve_locally = True
 
-server = app.server
+
 app.config.suppress_callback_exceptions = True
 
-
-# app.css.append_css({
-#     "external_url": "style.css"
-# })
